09da26d145544bbbee9907f5a113fd3005ce8dde
Cody Stewart <cjstewart88@gmail.com>
2020-01-11 19:14:39 -0500

    simplify the site to be a landing page

--- javascripts/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.nav-item').click(function() {
-    $('.nav-item').removeClass('active');
-    $(this).addClass('active');
-    $('section').hide();
-    $('#'+$(this).data('section')).show();
-  });
-});
